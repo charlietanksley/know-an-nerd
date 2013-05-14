10a5d859a747b5c3f3d2124c1de36efcf5c9ecfd
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-05-13 21:52:47 -0400

    roll quiz back into main file
    Everyone loves huge js files, right?

--- js/app.js
+++ js/app.js
@@ -129,6 +129,27 @@ $.domReady(function(){
     }
   };
 
+  var Question = function(theNerds) {
+    this.options = rara.randomSubset(theNerds, 4);
+    this.answer = rara.randomMember(this.options);
+
+    this.check = function(answer) {
+      return answer == this.answer.name;
+    };
+  };
+
+  var Quiz = function(question) {
+    this.run = function() {
+      $('.answer').on("click", function(e) {
+        answer = e.currentTarget.innerHTML;
+        if (question.check(answer)) {
+          $('#guess-result').html("Success!")
+        } else {
+          $('#guess-result').html("Learn a nerd!")
+        };
+      });
+    }
+  };
 
   // Experiment!
   $('#home').poke({
