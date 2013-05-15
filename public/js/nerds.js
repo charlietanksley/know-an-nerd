3965de87a4709479d51af589242eeee28b1e5292
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-05-15 07:46:18 -0400

    cashe the nerd list for a day to reduce api calls

--- public/js/nerds.js
+++ public/js/nerds.js
@@ -1,8 +1,12 @@
-$.ajax({
-  url: 'api/nerds',
-  method: 'get',
-  async: true,
-}).then(function(resp) {
-  $.cache('nerds').set('all-nerds', resp);
-});
+function getAllNerds() {
+  $.ajax({
+    url: 'api/nerds',
+    method: 'get',
+    async: true,
+  }).then(function(resp) {
+    $.cache('nerds').set('all-nerds', resp, 86400000);
+  });
+}
+
+$.cache('nerds').get('all-nerds') || getAllNerds()
 
