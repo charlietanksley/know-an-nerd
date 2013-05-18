c9d958ad67de37fcf37848823deca9921dd99e74
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-05-17 22:11:18 -0400

    add some functions to provide subsets of nerds
    It'll be good to be able to find all the nerds with pictures, for
    example.

--- public/js/nerds.js
+++ public/js/nerds.js
@@ -1,7 +1,11 @@
 var Nerds = {
-  allWithPictures: function() {
-    allNerds = $.cache('nerds').get('all-nerds');
-    nerds = $.v.reject(allNerds, function(nerd) {
+  allNerds: function() {
+    return $.cache('nerds').get('all-nerds');
+  },
+
+  withoutPictures: function() {
+    var nerds;
+    nerds = $.v.filter(this.allNerds(), function(nerd) {
       if (nerd.id == 'placeholder') {
         return nerd;
       };
@@ -10,12 +14,27 @@ var Nerds = {
     return nerds;
   },
 
+  allWithPictures: function() {
+    var nerds;
+    nerds = $.v.reject(this.allNerds(), function(nerd) {
+      if (nerd.id == 'placeholder') {
+        return nerd;
+      };
+    });
+
+    return nerds;
+  },
+
+  unknownNerds: function() {
+    return $.cache('nerds').get('unknown-nerds') ||
+      $.cache('nerds').set('unknown-nerds', Nerds.allWithPictures());
+  },
+
   getAllNerds: function() {
     microAjax("api/nerds", function (resp) {
       $.cache('nerds').set('all-nerds', JSON.parse(resp), 86400000);
     });
   }
 };
-
 
 $.cache('nerds').get('all-nerds') || Nerds.getAllNerds()
