aa1ea0d22e5822a93259853fae28e94f1966d5fa
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-10-30 15:05:40 -0400

    do the filtering work with a filter

--- frontend/app/js/controllers/nerds_index_controller.js
+++ frontend/app/js/controllers/nerds_index_controller.js
@@ -1,22 +1,3 @@
-angular.module("app").controller("NerdsIndexController", function($scope, nerds, NerdCacheService) {
-  var unknownNerds;
-
-  unknownNerds = function() {
-    var ids,
-        nerd,
-        knownNerds = $scope.knownNerds;
-
-    console.log(nerds);
-    ids = _.pluck(knownNerds, 'id');
-    console.log(ids);
-    return _.reject($scope.allNerds, function(nerd) {
-             if (_.contains(ids, nerd.id)) {
-               return nerd;
-             }
-           });
-  };
-
-  $scope.allNerds = nerds.data;
-  $scope.knownNerds = NerdCacheService.getKnown();
-  $scope.nerds = unknownNerds();
+angular.module("app").controller("NerdsIndexController", function($scope, nerds) {
+  $scope.nerds = nerds.data;
 });
