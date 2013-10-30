b66aaf0edde25b24a1ee8be9623ef5bdf8ce8254
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-10-30 12:49:42 -0400

    add a class to interact with localStorage

---
+++ frontend/app/js/lib/cache.js
@@ -0,0 +1,22 @@
+/* globals window */
+
+extend('KnowAnNerd.Cache', function(storageLocation) {
+  var Cache = {},
+  storage = storageLocation || window.localStorage;
+
+  console.log(storage);
+  Cache.get = function(key) {
+    var item;
+    item = localStorage.getItem(key);
+
+    return item;
+  };
+
+  Cache.set = function(key, value) {
+    localStorage.setItem(key, value);
+
+    return this;
+  };
+
+  return Cache;
+});
