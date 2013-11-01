5ea0cf5eb69ba27b08a39e7c7f9b53b3645c961b
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-01 13:43:45 -0400

    remove filters from index controller

--- frontend/app/js/controllers/nerds_index_controller.js
+++ frontend/app/js/controllers/nerds_index_controller.js
@@ -1,18 +1,3 @@
-angular.module("app").controller("NerdsIndexController", function($scope, nerds, $routeParams, $filter) {
-  var filter = function(data) {
-    var actualFilter,
-        selected = $routeParams['filter'];
-
-    if (selected === 'firstName') {
-      actualFilter = 'firstName';
-    } else if (selected === 'lastName') {
-      actualFilter = 'lastName';
-    } else {
-      actualFilter = 'filter';
-    }
-
-    return $filter(actualFilter)(data);
-  };
-
-  $scope.nerds = filter(nerds.data);
+angular.module("app").controller("NerdsIndexController", function($scope, nerds, $routeParams) {
+  $scope.nerds = nerds.data;
 });
