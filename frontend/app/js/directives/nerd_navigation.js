9501bdfc1668165202215d2602f4f7f8fee3613d
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-01 13:32:19 -0400

    nav focuses on search and filtering
    Still not implemented, but the html parts are in place.

--- frontend/app/js/directives/nerd_navigation.js
+++ frontend/app/js/directives/nerd_navigation.js
@@ -3,12 +3,6 @@ angular.module("app").directive("nerdNavigation", function() {
     restrict: 'E',
     templateUrl: "partials/nerd_navigation.html",
     link: function(scope, element, attrs) {
-      var filterOptions = [
-        { name: 'first name', filter: 'firstName' },
-        { name: 'last name', filter: 'lastName' }
-      ];
-
-      scope.filters = filterOptions;
     }
   };
 });
