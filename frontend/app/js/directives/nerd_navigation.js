5644b13dca04f2f840aec942d46e8b81ba1c3272
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-01 14:01:24 -0400

    remove unused link function in nav directive

--- frontend/app/js/directives/nerd_navigation.js
+++ frontend/app/js/directives/nerd_navigation.js
@@ -1,8 +1,6 @@
 angular.module("app").directive("nerdNavigation", function() {
   return {
     restrict: 'E',
-    templateUrl: "partials/nerd_navigation.html",
-    link: function(scope, element, attrs) {
-    }
+    templateUrl: "partials/nerd_navigation.html"
   };
 });
